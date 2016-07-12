cfb77ca084b12d1ca5a97468b3818bdcddfd5845
qinzuoyan <qinzuoyan@xiaomi.com>
2016-07-12 17:59:44 +0800

    implement a redis protocol proxy

--- replication/client_lib/src/rrdb_client_impl.cpp
+++ replication/client_lib/src/rrdb_client_impl.cpp
@@ -21,28 +21,6 @@ namespace dsn{ namespace apps{
 std::unordered_map<int, std::string> rrdb_client_impl::_client_error_to_string;
 std::unordered_map<int, int> rrdb_client_impl::_server_error_to_client;
 
-void rrdb_generate_key(dsn::blob& key, const std::string& hash_key, const std::string& sort_key)
-{
-    int len = 4 + hash_key.size() + sort_key.size();
-    char* buf = new char[len];
-    // hash_key_length is in big endian
-    *(int32_t*)buf = htobe32( (int32_t)hash_key.size() );
-    hash_key.copy(buf + 4, hash_key.size(), 0);
-    sort_key.copy(buf + 4 + hash_key.size(), sort_key.size(), 0);
-    std::shared_ptr<char> buffer(buf);
-    key.assign(buffer, 0, len);
-}
-
-uint64_t rrdb_key_hash(const ::dsn::blob& key)
-{
-    dassert(key.length() > 4, "key length must be greater than 4");
-    // hash_key_length is in big endian
-    int length = be32toh( *(int32_t*)(key.data()) );
-    dassert(key.length() >= 4 + length, "key length must be greater than 4 + hash_key length");
-    dsn::blob hash_key(key.buffer_ptr(), 4, length);
-    return dsn_crc64_compute(hash_key.data(), hash_key.length(), 0);
-}
-
 rrdb_client_impl::rrdb_client_impl(const char* cluster_name, const char* app_name)
     : _cluster_name(cluster_name), _app_name(app_name)
 {
