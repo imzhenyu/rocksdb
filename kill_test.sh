7f7a3bc74fade1593029b0d4e9372ae6b5139639
qinzuoyan <qinzuoyan@xiaomi.com>
2016-07-28 10:32:03 +0800

    improve kill_test scripts

--- kill_test.sh
+++ kill_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
-    echo "USAGE: $0 <meta-count> <replica-count> <app-name> <kill-type>"
+    echo "USAGE: $0 <meta-count> <replica-count> <app-name> <kill-type> <sleep-time>"
     echo "  kill-type: meta | replica | all"
     exit -1
 fi
@@ -10,6 +10,7 @@ META_COUNT=$1
 REPLICA_COUNT=$2
 APP_NAME=$3
 KILL_TYPE=$4
+SLEEP_TIME=$5
 if [ "$KILL_TYPE" != "meta" -a "$KILL_TYPE" != "replica" -a "$KILL_TYPE" != "all" ]
 then
     echo "ERROR: invalid kill-type, should be: meta | replica | all"
@@ -18,12 +19,14 @@ fi
 
 CUR_DIR=`pwd`
 LEASE_TIMEOUT=20
-WAIT_SECONDS=0
+UNHEALTHY_TIMEOUT=1200
+KILL_META_PROB=10
 SHELL_INPUT=.kill_test.shell.input
 SHELL_OUTPUT=.kill_test.shell.output
 echo "cluster_info" >$SHELL_INPUT
 echo "nodes" >>$SHELL_INPUT
 echo "app $APP_NAME -detailed" >>$SHELL_INPUT
+WAIT_SECONDS=0
 while true
 do
   if ! ps -ef | grep "rrdb_kill_test" |  grep -v grep &>/dev/null
@@ -56,7 +59,7 @@ do
   UNHEALTHY_COUNT=`cat $SHELL_OUTPUT | awk '{print $3}' | grep '/' | grep -o '^[0-9]*' | grep -v '3' | wc -l`
   if [ $UNALIVE_COUNT -gt 0 -o $PARTITION_COUNT -eq 0 -o $UNHEALTHY_COUNT -gt 0 ]
   then
-    if [ $WAIT_SECONDS -gt 1200 ]
+    if [ $WAIT_SECONDS -gt $UNHEALTHY_TIMEOUT ]
     then
       echo "[`date`] not healthy for $WAIT_SECONDS seconds, stop rrdb_kill_test and cluster..."
       echo "---------------------------"
@@ -86,7 +89,7 @@ do
   then
       TYPE=relica
   else
-      R=$((RANDOM % 10))
+      R=$((RANDOM % KILL_META_PROB))
       if [ $R -eq 0 ]
       then
           TYPE=meta
@@ -103,7 +106,7 @@ do
       TASK_ID=$((RANDOM % REPLICA_COUNT + 1))
   fi
   ###
-  SECONDS=$((RANDOM % 300 + 1))
+  SECONDS=$((RANDOM % SLEEP_TIME + 1))
   echo "[`date`] sleep for $SECONDS seconds to stop $TYPE #$TASK_ID"
   sleep $SECONDS
   ###
@@ -120,7 +123,7 @@ do
   echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   echo
 
-  echo "[`date`] sleep for 20 seconds, and wait for healthy..."
+  echo "[`date`] first sleep for $LEASE_TIMEOUT seconds, and then wait for healthy..."
   echo
   sleep $LEASE_TIMEOUT
   WAIT_SECONDS=$LEASE_TIMEOUT
